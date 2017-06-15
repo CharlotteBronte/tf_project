e245a6b48240957f81fa6f258b5c58a6a7b99acc
liuluxin <0_0mirror@sina.com>
2017-06-15 11:57:35 +0800

    update tf board demo

--- tf_notebook/base_conception/tensor_board_demo.py
+++ tf_notebook/base_conception/tensor_board_demo.py
@@ -7,62 +7,183 @@
 @param:
 @output:
 """
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""A simple MNIST classifier which displays summaries in TensorBoard.
+ This is an unimpressive MNIST model, but it is a good example of using
+tf.name_scope to make a graph legible in the TensorBoard graph explorer, and of
+naming summary tags so that they are grouped meaningfully in TensorBoard.
+It demonstrates the functionality of every TensorBoard dashboard.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import tensorflow as tf
-import numpy as np
 
-#输入数据
-x_data = np.linspace(-1,1,300)[:, np.newaxis]
-noise = np.random.normal(0,0.05, x_data.shape)
-y_data = np.square(x_data)-0.5+noise
+from tensorflow.examples.tutorials.mnist import input_data
 
-#输入层
-with tf.name_scope('input_layer'): #输入层。将这两个变量放到input_layer作用域下，tensorboard会把他们放在一个图形里面
-    xs = tf.placeholder(tf.float32, [None, 1], name = 'x_input') # xs起名x_input，会在图形上显示
-    ys = tf.placeholder(tf.float32, [None, 1], name = 'y_input') # ys起名y_input，会在图形上显示
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data '
+                     'for unit testing.')
+flags.DEFINE_integer('max_steps', 1000, 'Number of steps to run trainer.')
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
+flags.DEFINE_float('dropout', 0.9, 'Keep probability for training dropout.')
+flags.DEFINE_string('data_dir', '/tmp/data', 'Directory for storing data')
+flags.DEFINE_string('summaries_dir', '/tmp/mnist_logs', 'Summaries directory')
 
-#隐层
-with tf.name_scope('hidden_layer'): #隐层。将隐层权重、偏置、净输入放在一起
-    with tf.name_scope('weight'): #权重
-        W1 = tf.Variable(tf.random_normal([1,10]))
-        tf.summary.histogram('hidden_layer/weight', W1)
-    with tf.name_scope('bias'): #偏置
-        b1 = tf.Variable(tf.zeros([1,10])+0.1)
-        tf.summary.histogram('hidden_layer/bias', b1)
-    with tf.name_scope('Wx_plus_b'): #净输入
-        Wx_plus_b1 = tf.matmul(xs,W1) + b1
-        tf.summary.histogram('hidden_layer/Wx_plus_b',Wx_plus_b1)
-output1 = tf.nn.relu(Wx_plus_b1)
 
-#输出层
-with tf.name_scope('output_layer'): #输出层。将输出层权重、偏置、净输入放在一起
-    with tf.name_scope('weight'): #权重
-        W2 = tf.Variable(tf.random_normal([10,1]))
-        tf.summary.histogram('output_layer/weight', W2)
-    with tf.name_scope('bias'): #偏置
-        b2 = tf.Variable(tf.zeros([1,1])+0.1)
-        tf.summary.histogram('output_layer/bias', b2)
-    with tf.name_scope('Wx_plus_b'): #净输入
-        Wx_plus_b2 = tf.matmul(output1,W2) + b2
-        tf.summary.histogram('output_layer/Wx_plus_b',Wx_plus_b2)
-output2 = Wx_plus_b2
+def train():
+  # Import data
+  mnist = input_data.read_data_sets(FLAGS.data_dir,
+                                    one_hot=True,
+                                    fake_data=FLAGS.fake_data)
 
-#损失
-with tf.name_scope('loss'): #损失
-    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-output2),reduction_indices=[1]))
-    tf.summary.scalar('loss',loss)
-with tf.name_scope('train'): #训练过程
-    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+  sess = tf.InteractiveSession()
 
-#初始化
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
-merged = tf.summary.merge_all() #将图形、训练过程等数据合并在一起
-writer = tf.summary.FileWriter('logs',sess.graph) #将训练日志写入到logs文件夹下
+  # Create a multilayer model.
 
-#训练
-for i in range(1000):
-    sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
-    if(i%50==0): #每50次写一次日志
-        result = sess.run(merged,feed_dict={xs:x_data,ys:y_data}) #计算需要写入的日志数据
-        writer.add_summary(result,i) #将日志数据写入文件
+  # Input placehoolders
+  with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, [None, 784], name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
+
+  with tf.name_scope('input_reshape'):
+    image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
+    tf.image_summary('input', image_shaped_input, 10)
+
+  # We can't initialize these variables to 0 - the network will get stuck.
+  def weight_variable(shape):
+    """Create a weight variable with appropriate initialization."""
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+  def bias_variable(shape):
+    """Create a bias variable with appropriate initialization."""
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+  def variable_summaries(var, name):
+    """Attach a lot of summaries to a Tensor."""
+    with tf.name_scope('summaries'):
+      mean = tf.reduce_mean(var)
+      tf.scalar_summary('mean/' + name, mean)
+      with tf.name_scope('stddev'):
+        stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
+      tf.scalar_summary('sttdev/' + name, stddev)
+      tf.scalar_summary('max/' + name, tf.reduce_max(var))
+      tf.scalar_summary('min/' + name, tf.reduce_min(var))
+      tf.histogram_summary(name, var)
+
+  def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
+    """Reusable code for making a simple neural net layer.
+    It does a matrix multiply, bias add, and then uses relu to nonlinearize.
+    It also sets up name scoping so that the resultant graph is easy to read,
+    and adds a number of summary ops.
+    """
+    # Adding a name scope ensures logical grouping of the layers in the graph.
+    with tf.name_scope(layer_name):
+      # This Variable will hold the state of the weights for the layer
+      with tf.name_scope('weights'):
+        weights = weight_variable([input_dim, output_dim])
+        variable_summaries(weights, layer_name + '/weights')
+      with tf.name_scope('biases'):
+        biases = bias_variable([output_dim])
+        variable_summaries(biases, layer_name + '/biases')
+      with tf.name_scope('Wx_plus_b'):
+        preactivate = tf.matmul(input_tensor, weights) + biases
+        tf.histogram_summary(layer_name + '/pre_activations', preactivate)
+      activations = act(preactivate, 'activation')
+      tf.histogram_summary(layer_name + '/activations', activations)
+      return activations
+
+  hidden1 = nn_layer(x, 784, 500, 'layer1')
+
+  with tf.name_scope('dropout'):
+    keep_prob = tf.placeholder(tf.float32)
+    tf.scalar_summary('dropout_keep_probability', keep_prob)
+    dropped = tf.nn.dropout(hidden1, keep_prob)
+
+  y = nn_layer(dropped, 500, 10, 'layer2', act=tf.nn.softmax)
+
+  with tf.name_scope('cross_entropy'):
+    diff = y_ * tf.log(y)
+    with tf.name_scope('total'):
+      cross_entropy = -tf.reduce_mean(diff)
+    tf.scalar_summary('cross entropy', cross_entropy)
+
+  with tf.name_scope('train'):
+    train_step = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(
+        cross_entropy)
+
+  with tf.name_scope('accuracy'):
+    with tf.name_scope('correct_prediction'):
+      correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    with tf.name_scope('accuracy'):
+      accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    tf.scalar_summary('accuracy', accuracy)
+
+  # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+  merged = tf.merge_all_summaries()
+  train_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/train',
+                                        sess.graph)
+  test_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/test')
+  tf.initialize_all_variables().run()
+
+  # Train the model, and also write summaries.
+  # Every 10th step, measure test-set accuracy, and write test summaries
+  # All other steps, run train_step on training data, & add training summaries
+
+  def feed_dict(train):
+    """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
+    if train or FLAGS.fake_data:
+      xs, ys = mnist.train.next_batch(100, fake_data=FLAGS.fake_data)
+      k = FLAGS.dropout
+    else:
+      xs, ys = mnist.test.images, mnist.test.labels
+      k = 1.0
+    return {x: xs, y_: ys, keep_prob: k}
+
+  for i in range(FLAGS.max_steps):
+    if i % 10 == 0:  # Record summaries and test-set accuracy
+      summary, acc = sess.run([merged, accuracy], feed_dict=feed_dict(False))
+      test_writer.add_summary(summary, i)
+      print('Accuracy at step %s: %s' % (i, acc))
+    else:  # Record train set summaries, and train
+      if i % 100 == 99:  # Record execution stats
+        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+        run_metadata = tf.RunMetadata()
+        summary, _ = sess.run([merged, train_step],
+                              feed_dict=feed_dict(True),
+                              options=run_options,
+                              run_metadata=run_metadata)
+        train_writer.add_run_metadata(run_metadata, 'step%d' % i)
+        train_writer.add_summary(summary, i)
+        print('Adding run metadata for', i)
+      else:  # Record a summary
+        summary, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+        train_writer.add_summary(summary, i)
+
+
+def main(_):
+  if tf.gfile.Exists(FLAGS.summaries_dir):
+    tf.gfile.DeleteRecursively(FLAGS.summaries_dir)
+  tf.gfile.MakeDirs(FLAGS.summaries_dir)
+  train()
+
+
+if __name__ == '__main__':
+  tf.app.run()
