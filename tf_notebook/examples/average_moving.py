9dbe9f490f9b94f02d615f50087c1ad58681906b
liuluxin <0_0mirror@sina.com>
2017-06-19 20:23:15 +0800

    add av_mv print

--- tf_notebook/examples/average_moving.py
+++ tf_notebook/examples/average_moving.py
@@ -19,23 +19,19 @@ with tf.Session() as sess:
     # 初始化
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
-    print
-    sess.run([v1, ema.average(v1)])
+    print sess.run([v1, ema.average(v1)])
 
     # 更新变量v1的取值
     sess.run(tf.assign(v1, 5))
     sess.run(maintain_averages_op)
-    print
-    sess.run([v1, ema.average(v1)])
+    print sess.run([v1, ema.average(v1)])
 
     # 更新step和v1的取值
     sess.run(tf.assign(step, 10000))
     sess.run(tf.assign(v1, 10))
     sess.run(maintain_averages_op)
-    print
-    sess.run([v1, ema.average(v1)])
+    print sess.run([v1, ema.average(v1)])
 
     # 更新一次v1的滑动平均值
     sess.run(maintain_averages_op)
-    print
-    sess.run([v1, ema.average(v1)])
+    print sess.run([v1, ema.average(v1)])
