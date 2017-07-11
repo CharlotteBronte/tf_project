873dbe65d10f180d85c60d3c6f87f2665bad5f76
panagdaxing <0_0mirror@sina.com>
2017-07-11 13:49:25 +0800

    add data

--- nlp/word_embedding/run.sh
+++ nlp/word_embedding/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=2
-python word_2_vec.py word_embedding_test.ini
+export CUDA_VISIBLE_DEVICES=0,1
+
+#####################################################################
+##测试数据
+##test data
+#####################################################################
+
+SCRIPT="./script"
+DATA="./data"
+CONFIG="./configs"
+sh $SCRIPT/build_dict.sh  $DATA/test/seg_qa
+python $SCRIPT/word_2_vec.py $CONFIG/word_embedding_test.ini
+
+#####################################################################
+##全量qa训练数据
+##whole train data
+#####################################################################
+
